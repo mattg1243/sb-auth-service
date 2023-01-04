311d0431c38033e1ffed5b0439f66cfa00d1516e
Matt Gallucci <mattgallucci97@gmail.com>
2023-01-04 00:26:37 -0800

    added route for loggin out user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { indexHandler, loginHandler } from '../handlers';
+import { logoutHandler } from '../handlers/index';
 
 const router = Router();
 
 router.get('/', indexHandler);
 
 router.post('/login', loginHandler);
+router.get('/logout', logoutHandler);
 
 export default router;
